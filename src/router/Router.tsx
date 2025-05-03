993982bc07e8175f38e024558684a805ffd2ce9a
hwanggyuun <gka365@naver.com>
2025-05-03 20:53:09 +0900

    header 화면과 chatting 화면 구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from '../page/main/Main';
+import Chatting from '../page/chatting/Chatting';
 const Routers = () => {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Main />,
+    },
+    {
+      path: '/chatting',
+      element: <Chatting />,
     },
   ]);
 
