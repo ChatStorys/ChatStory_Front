22ccd1f95dd4e4066f5cb659d580a1429e8c6864
hwanggyuun <gka365@naver.com>
2025-05-05 22:54:26 +0900

    scroll 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Routers from './router/Router.tsx';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    overflow: hidden; /* 👈 전역 스크롤 제거 */
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: #e4e0e1;
+  }
+`;
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <>
     <Routers />
-  </StrictMode>,
+    <GlobalStyle />
+  </>,
 );
--- src/component/chatcontent/Chatcontent.tsx
+++ src/component/chatcontent/Chatcontent.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 100%;
-  margin-top: 40px;
+  height: 750px;
+  margin: 40px 0;
+  overflow: scroll;
 `;
 const Chatcontainer = styled.div`
   display: flex;
