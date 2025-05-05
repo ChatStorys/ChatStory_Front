7be697f168d6b9ab096f75f3b00c77d571e47dd6
hwanggyuun <gka365@naver.com>
2025-05-05 22:54:00 +0900

    feature footer

--- src/page/chatting/Chatting.tsx
+++ src/page/chatting/Chatting.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from '../../component/header/Header';
 import Chatcontent from '../../component/chatcontent/Chatcontent';
-import Footer from '../../component/footer/footer';
+import Footer from '../../component/footer/Footer';
 const Container = styled.div`
   background: #e4e0e1;
   padding: 0 126px;
@@ -19,14 +19,32 @@ const Chatting: React.FC = () => {
         '들어가도 되는 걸까...그는 한숨을 내쉬고 첫 발을 디뎠다. 몇 바퀴를 돌며 내려가자, 갑자기 눈앞에 작은 방이 나타났다. 방 안에는 촛불 하나가 깜빡거리고 있었고, 그 밑에 낡은 책상과 책 한 권이 놓여 있었다. 소년은 다가가 책을 펼쳤다. 책장에는 단 한 줄,',
       User: '너는 여기서 깨어나야 한다',
     },
+    {
+      LLM_Model:
+        '들어가도 되는 걸까...그는 한숨을 내쉬고 첫 발을 디뎠다. 몇 바퀴를 돌며 내려가자, 갑자기 눈앞에 작은 방이 나타났다. 방 안에는 촛불 하나가 깜빡거리고 있었고, 그 밑에 낡은 책상과 책 한 권이 놓여 있었다. 소년은 다가가 책을 펼쳤다. 책장에는 단 한 줄,',
+      User: '너는 여기서 깨어나야 한다',
+    },
+    {
+      LLM_Model:
+        '들어가도 되는 걸까...그는 한숨을 내쉬고 첫 발을 디뎠다. 몇 바퀴를 돌며 내려가자, 갑자기 눈앞에 작은 방이 나타났다. 방 안에는 촛불 하나가 깜빡거리고 있었고, 그 밑에 낡은 책상과 책 한 권이 놓여 있었다. 소년은 다가가 책을 펼쳤다. 책장에는 단 한 줄,',
+      User: '너는 여기서 깨어나야 한다',
+    },
+    {
+      LLM_Model:
+        '들어가도 되는 걸까...그는 한숨을 내쉬고 첫 발을 디뎠다. 몇 바퀴를 돌며 내려가자, 갑자기 눈앞에 작은 방이 나타났다. 방 안에는 촛불 하나가 깜빡거리고 있었고, 그 밑에 낡은 책상과 책 한 권이 놓여 있었다. 소년은 다가가 책을 펼쳤다. 책장에는 단 한 줄,',
+      User: '너는 여기서 깨어나야 한다',
+    },
+
     { LLM_Model: '뭐야... 이게...', User: '그 순간, 책장이 저절로 넘어가기 시작했다. 새로운 글자가 피처럼 번져나오더니, 소년의 이름을 새겼다' },
   ];
   return (
-    <Container>
-      <Header title="소설 제목" />
-      <Chatcontent chatcontent={content} />
+    <>
+      <Container>
+        <Header title="소설 제목" />
+        <Chatcontent chatcontent={content} />
+      </Container>
       <Footer />
-    </Container>
+    </>
   );
 };
 
